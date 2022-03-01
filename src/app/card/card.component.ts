a6dfb5eb09397474c8ee3b9f13f037dfe747f9e4
olepunchy <olepunchy@gmail.com>
2022-03-01 07:03:48 -0600

    Updating titlebar

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { CountdownComponent } from 'ngx-countdown';
+import { Title } from '@angular/platform-browser';
+import { CountdownComponent, CountdownEvent } from 'ngx-countdown';
 
 @Component({
   selector: 'app-card',
@@ -11,7 +12,29 @@ export class CardComponent implements OnInit {
 
   totalSeconds = 1500; // 25 minutes
 
-  constructor() { }
+  titleTime = "25:00";
+
+  @ViewChild('cd', { static: false }) private countdown: CountdownComponent;
+
+  start() {
+    this.countdown.begin();
+  }
+
+  stop() {
+    this.countdown.stop();
+  }
+
+  reset() {
+    this.countdown.restart();
+  }
+
+  handleCountDown(event: CountdownEvent) {
+    this.titleService.setTitle(event.text);
+  }
+
+  constructor(private titleService: Title) { 
+    this.titleService.setTitle(this.titleTime);
+  }
 
   ngOnInit(): void {
   }
